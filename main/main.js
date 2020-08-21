e07e5623da33110ed5dddf65ec6f40ab626f5e19
Yusuf69Hendricks <yusuf69hendricks@gmail.com>
2020-08-21 15:13:48 +0200

    added both html and css to  FAQ section

--- main/main.js
+++ main/main.js
@@ -23,3 +23,20 @@ function scrollFunction() {
     document.getElementById("navbar").style.backgroundColor = "#16325b";
   }
 }
+
+function BtnFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("readmore");
+
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
