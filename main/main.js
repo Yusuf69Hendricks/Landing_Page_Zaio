d405344150429afffecac6b9c40820bbd26c4b1d
Yusuf69Hendricks <yusuf69hendricks@gmail.com>
2020-08-21 19:47:21 +0200

    added media queries and made website fully responsive

--- main/main.js
+++ main/main.js
@@ -11,32 +11,35 @@ function myFunction() {
 
 // COLOR CHNAGE
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "0";
-    document.getElementById("navbar").style.backgroundColor = "#ddd";
-    // document.getElementById("navbar").style.font = "#16325b"
-  } else {
-    document.getElementById("navbar").style.padding = "0";
-    document.getElementById("navbar").style.backgroundColor = "#16325b";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("navbar").style.padding = "0";
+//     document.getElementById("navbar").style.backgroundColor = "white";
+//     document.getElementById("navbar").style.color = "red";
+    
+//     // document.getElementById("navbar").style.font = "#16325b"
+//   } else {
+//     document.getElementById("navbar").style.padding = "0";
+//     document.getElementById("navbar").style.backgroundColor = "#243b75";
+//     document.getElementById("navbar").style.color = "yellow";
+//   }
+// }
 
-function BtnFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("readmore");
+// function BtnFunction() {
+//     var dots = document.getElementById("dots");
+//     var moreText = document.getElementById("more");
+//     var btnText = document.getElementById("readmore");
 
   
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "inline";
-    }
-  }
+//     if (dots.style.display === "none") {
+//       dots.style.display = "inline";
+//       btnText.innerHTML = "Read more"; 
+//       moreText.style.display = "none";
+//     } else {
+//       dots.style.display = "none";
+//       btnText.innerHTML = "Read less"; 
+//       moreText.style.display = "inline";
+//     }
+//   }
